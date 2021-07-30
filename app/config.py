956fe265948115241ae94d16e9fa5ba274ee54fb
Yash M Patel <85469726+Yash-2903@users.noreply.github.com>
2021-07-30 14:58:33 -0500

    Update: Modify config.py file

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
-"""Flask configuration."""
+"""Flask config."""
 
-MYSQL_DATABASE_HOST = 'db'
-MYSQL_DATABASE_USER = 'root'
-MYSQL_DATABASE_PASSWORD = 'root'
-MYSQL_DATABASE_PORT = 3306
-MYSQL_DATABASE_DB = 'citiesData'
+
+class Config:
+    MYSQL_DATABASE_HOST = 'db'
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = 'root'
+    MYSQL_DATABASE_PORT = 3306
+    MYSQL_DATABASE_DB = 'citiesData'
